f044886aa6bfb06e32de16f0448d47aceadee885
Emmanuel Pire <piremmanuel@gmail.com>
2016-06-05 15:52:50 +0200

    Hover for title too

--- public/js/main.js
+++ public/js/main.js
@@ -105,14 +105,14 @@ setInterval(refreshTimer, 60000);
 
 $(window).on('resize', scrollText);
 
-$('.text').delegate('span', 'mouseenter', function(){
+$('.hover-words').delegate('span', 'mouseenter', function(){
 	var index = $(this).data('i');
 	bubble.show(this, index);
 });
-$('.text').delegate('span', 'mouseleave', function(){
+$('.hover-words').delegate('span', 'mouseleave', function(){
 	bubble.hide();
 });
-$('.text').delegate('span', 'click', function(e){
+$('.hover-words').delegate('span', 'click', function(e){
 	var index = $(this).data('i');
 	e.preventDefault();
 	TweetStore.get(index, function(tw){
